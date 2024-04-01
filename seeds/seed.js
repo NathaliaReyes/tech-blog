65768c258dfe1658cdd9be68b07e1a7e5a9a3b92
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-04-01 10:16:08 -0600

    Data added to seed all the tables in the db. Database seeded

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Post.bulkCreate(postData);
+
+    await Comment.bulkCreate(commentData);
+
+    process.exit(0);
+}
+
+seedDatabase();
