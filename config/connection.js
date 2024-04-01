68b40128c01b36ab2031f7fd0f935aa2050a79a0
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-04-01 08:39:57 -0600

    connection.js modified

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
+let sequelize;
+if(process.env.DB_URL){
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -12,5 +12,6 @@ const sequelize = process.env.DB_URL
       dialect: 'postgres',
     }
   );
+}
 
 module.exports = sequelize;
