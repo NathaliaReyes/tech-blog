0070a67613f371b3a7171742a6c29dbe53fc797b
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-04-03 11:55:01 -0600

    Styling Login and Signup pages

--- public/js/signup.js
+++ public/js/signup.js
@@ -22,3 +22,11 @@ const signupFormHandler = async (event) => {
 
 document
 .querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+const labels = document.querySelectorAll('.form-group label');
+    labels.forEach(label => {
+        label.innerHTML = label.innerText
+            .split('')
+            .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+            .join('')
+    });
