d2d2b773fdfe74da93b536d5aec2e5a7da362a66
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-04-04 11:54:17 -0600

    debugging purposes

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,17 @@ require('dotenv').config();
 
 let sequelize;
 if(process.env.DATABASE_URL){
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 app.get('/favicon.ico', (req, res) => res.status(204));
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5432;
 
 const hbs = exphbs.create({ helpers });
 
